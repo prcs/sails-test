2605f2b0ee681e2e19a6503f32537459ca273422
Peterson Rodrigo Da Costa Silva <peterson.silva@montreal.com.br>
2017-07-26 13:32:06 -0300

    ok

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,19 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  mongoDev: {
+    adapter: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    //user: 'username', //optional
+    //password: 'password', //optional
+    database: 'your_mongo_db_name_here' //optional
+  },
+
+   mongoProd: {
+    adapter: 'sails-mongo',
+    url: process.env.mongoUrl
+  },
 
   /***************************************************************************
   *                                                                          *
